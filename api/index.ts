765611c4fdcfd94c0823f605cabbf62785972930
Ganesh Ghatti <ganeshghatti6@gmail.com>
2025-04-29 21:45:41 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -20,13 +20,12 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+
 // Middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-
-
-app.use(cors());
 
 // Global rate limiting - applies to all routes
 const globalLimiter = rateLimit({
