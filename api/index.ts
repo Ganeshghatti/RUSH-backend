45388367d5afe824b7bfff5382f94b3707b51413
Ganesh Ghatti <ganeshghatti6@gmail.com>
2025-04-29 21:50:14 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -20,8 +20,12 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "https://app.rushdr.com",
+    credentials: true, // ⬅️ Must be true to allow cookies/auth headers
+  })
+);
 // Middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
