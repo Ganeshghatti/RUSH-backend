0bc49d40c3d78400ec42f52670117f76fc7f5636
aasuyadav1 <aasuy284@gmail.com>
2025-06-17 21:05:11 +0530

    add: dashboard

--- api/routes/onboard/patient.ts
+++ api/routes/onboard/patient.ts
@@ -1,9 +1,11 @@
 import { verifyToken, checkRole } from "../../middleware/auth-middleware";
 import { Router } from "express";
-import { patientOnboard } from "../../controller/patient/patient";
+import { patientOnboard, getPatientDashboard, getAppointmentsDoctorForPatient } from "../../controller/patient/patient";
 
 const router = Router();
 
 router.route("/onboard/patient/:userId").put(verifyToken, checkRole("patient"), patientOnboard);
+router.route("/dashboard").get(verifyToken, checkRole("patient"), getPatientDashboard);
+router.route("/appointments/doctor").get(verifyToken, checkRole("patient"), getAppointmentsDoctorForPatient);
 
-export default router;
+export default router; 
